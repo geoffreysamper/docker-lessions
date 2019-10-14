ad8ed693f164b0a0aa9d56eb7a65dbf03266dc61
Geoffrey Samper <g.samper@tmg.nl>
2019-10-14 20:43:25 +0200

    added cmds

---
+++ cmds-and-entrypoint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cmds_and_entrypoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+		
+	    Console.Write("args are: ");	
+            Console.WriteLine(string.Join(" ", args));
+        }
+    }
+}
