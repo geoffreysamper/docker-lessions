840899e6a055d31184580dd189f73cd659124949
Geoffrey Samper <g.samper@tmg.nl>
2019-10-14 21:33:13 +0200

    added volume example

---
+++ volume-console-app/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace volume_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "./default_content.txt";
+            if (args.Length > 0){
+                filePath = args[0];
+            }
+           
+
+           var text =  System.IO.File.ReadAllText(args[0]);
+           Console.WriteLine("Content of file :");
+           Console.WriteLine(text); 
+
+        }
+    }
+}
